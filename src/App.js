77e32382519f29ad87299ec37eb8b047e84d99b3
Juan M <jromano743@gmail.com>
2022-06-22 20:24:29 -0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { TodoCounter } from "./components/TodoCounter";
+import { TodoSearch } from "./components/TodoSearch";
+import { TodoList } from "./components/TodoList";
+import { TodoItem } from "./components/TodoItem";
+import { CreateTodoButton } from "./components/CreateTodoButton";
+//import './App.css';
+
+const todos = [
+  {text: "Comprar huevos", completed: false},
+  {text: "Farmear el pase", completed: true},
+  {text: "Terminar el bot", completed: false},
+  {text: "Armar los modelos del jugador y la paloma", completed: false},
+] 
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <React.Fragment>
+      <TodoCounter />    
+      <TodoSearch />
+
+      <TodoList>
+        { todos.map(item =>( 
+          <TodoItem key={item.text} text={item.text} completed={item.completed}/> 
+        )) }
+      </TodoList>
+
+      <CreateTodoButton />      
+   </React.Fragment>
   );
 }
 
---
+++ src/components/TodoCounter.js
@@ -0,0 +1,10 @@
+import React from "react";
+import '../styles/TodoCounter.css';
+
+function TodoCounter(){
+    return(
+        <h2 className="TodoCounter"> Has complentado 2 de 3 ToDos</h2>
+    )
+}
+
+export { TodoCounter };
